16cb9854065986ab403158b4e2ed8dfc570e47a9
Ronald Kroening <30726351+RonaldKroening@users.noreply.github.com>
2021-11-07 19:22:00 -0500

    Add files via upload

---
+++ syntext.jl
@@ -0,0 +1,262 @@
+# Get dataset
+using Pkg
+using Word2Vec
+using CSV
+using WordTokenizers
+using Random
+
+function activation(Weighted_Inputs,bias)
+    active_inputs = []
+    for x in Weighted_Inputs
+        t = tanh(x)
+        if(t > 0)
+            append!(active_inputs,t+bias)
+        end
+    end
+    return active_inputs
+end
+
+function add_arrays(A,B)
+    a = length(A)
+    b = length(B)
+    C = []
+    if(a > b)
+        for x in 1:b
+            append!(C,A[x]+B[x])
+        end
+    else
+        for x in 1:a
+            append!(C,A[x]+B[x])
+        end
+    end
+    return C
+end
+
+function average(vec)
+    i = 0
+    count  = 0
+    for num in vec
+        i+= num
+        count+=1
+    end
+    return i/count
+end
+
+function error(expected, predicted)
+    len = length(expected)
+    tot = []
+    for i in 1:len
+        append!(tot,expected[i]-predicted[i])
+    end
+    sum = 0
+    count = 0
+    for i in in 1:len
+        sum += tot[i]
+        count+=1
+    end
+    return sum/count
+end
+
+function deriv(x)
+    return x*(1-x)
+end
+
+function format_word(word)
+    w = lowercase(word)
+    newWord = ""
+    lets = "abcdefghijklmnopqrstuvwxyz"
+    for i in w
+        if i in lets
+            newWord *= i
+        end
+    end
+    return newWord
+end
+
+function generate_first_input_weights(numputs)
+    input_weights = []
+    for x = 1 : numputs
+        r = random_float()
+        append!(input_weights,r)
+    end
+    return input_weights
+end
+
+function hidden_layer(input,weights,learningRate, iterations, bias, expectation)
+    W = weights
+    er = 0
+    for i in 1:iterations
+        Weighted_Inputs = multiply_arrays(input,W)
+        weighted_sum = sum(Weighted_Inputs)
+        size = length(Weighted_Inputs) / 2
+        hiddenLayer = []
+        for i in 1:size
+            append!(hiddenLayer,weighted_sum+bias)
+        end
+        activeLayer = activation(hidden_layer,bias)
+        output = []
+        for i in 1:5
+            aSum = sum(activeLayer)
+            append!(output,relu(aSum))
+        end
+        realerror = error(expectation, output)
+        er = realerror
+        println("Predicted ",output, " with error rate",er)
+        W = update_weights(W,learningRate,realerror)
+    end
+
+end
+
+function multiply_arrays(A,B)
+    a = length(A)
+    b = length(B)
+    C = []
+    if(a > b)
+        for x in 1:b
+            append!(C,A[x]*B[x])
+        end
+    else
+        for x in 1:a
+            append!(C,A[x]*B[x])
+        end
+    end
+    return C
+end
+
+function random_float()
+    Random.seed!()
+    randfloat = rand()
+    if randfloat === 0.0
+        randfloat = 1.0
+    end
+    return randfloat
+end
+
+function relu(x)
+    if(x >0)
+        return x
+    else
+        return 0
+    end
+end
+
+function sum(A)
+    sum = 0
+    for a in A
+        sum +=A
+    end
+    return sum
+end
+
+function train(learningRate, epochs, bias)
+    x=1
+    csv_reader = CSV.File("dataset.csv")
+    rows = csv_reader.rows
+    columns = csv_reader.columns
+    vecAvgs = Dict{string,float}
+    weights = []
+    for row in csv_reader
+        expected_output = []
+        value = row.ScienceFiction
+        append!(expected_output,value)
+        value = row.Fantasy
+        append!(expected_output,value)
+        value = row.News
+        append!(expected_output,value)
+        value = row.ScientificPaper
+        append!(expected_output,value)
+        value = row.Biography
+        append!(expected_output,value)
+        s = row.Text
+        tr = split(s)
+        input = []
+        for t in tr
+            try
+                i = vecAvgs[t]
+                g = average(i)
+                if(g != 0.0)
+                    append!(input, g)
+                end
+            catch
+                i = wtvt(t)
+                g = average(i)
+                if(g != 0.0)
+                    append!(input, g)
+                end
+            end
+        end
+        println("Expected ",expected_output);
+        weights = generate_first_input_weights()
+        weights = hidden_layer(input,weights,learningRate,epochs, bias, expected)
+        break
+    end
+    return weights
+end
+
+function update_weight(weight,learningRate,error)
+    derivE = deriv(error)
+    derivOldW = deriv(weight)
+    div = derivE/derivOldW
+    sub = learningRate * div
+    return weight - sub
+end
+
+function update_weights(weights, learningRate,error)
+    new_weights = []
+    for w in weights
+        nw = update_weight(w,learningRate,error)
+        append!(new_weights,nw)
+    end
+    return new_weights
+end
+
+function wtvt(word)
+    model = wordvectors("text8-vec.txt")
+    w = lowercase(word)
+    try
+        return get_vector(model,w)
+    catch
+        return 0.0
+    end
+end
+
+learningRate = .6
+epochs = 10
+bias = .88
+# weights = train(learningRate,epochs,bias);
+# print("hi")
+#Use the weights for further analysis
+# print("hello world")
+
+function obtain_vectors()
+    csv_reader = CSV.File("dataset.csv")
+    vecAvgs = Dict()
+    f = open("usedvectors.txt","w")
+    count = 1
+    for row in csv_reader
+        println("Row ",count)
+        s = row.Text
+        tr = split(s)
+        input = []
+        for t in tr
+            g = 0
+            write(f,t)
+            write(f,"\n")
+            try
+                i = vecAvgs[t]
+                g = average(i)
+            catch
+                i = wtvt(t)
+                g = average(i)
+                vecAvgs[string(t)] = g
+            end
+            append!(input,t)
+            g1 = string(g)
+            write(f,g1)
+            write(f,"\n")
+        end
+        count+=1
+    end
+    f.close()
+end
+obtain_vectors();
